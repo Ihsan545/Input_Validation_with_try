2ef03f4d898a652baf4136abc78988a1c5f98fe1
anwar <Ihsan545>
2020-09-21 19:53:21 -0500

    Second commit

--- Validation_with_try/Input_Validation_with_try/test_average_negative_input.py
+++ Validation_with_try/Input_Validation_with_try/test_average_negative_input.py
@@ -8,13 +8,13 @@ import unittest
 import unittest.mock as result
 from Input_Validation_with_try import input_validation
 
+
 class MyTestCase(unittest.TestCase):
 
     def test_average(self):
-        with result.patch('builtins.input', side_effect=[95, -85, -90]):
+        with result.patch('builtins.input', side_effect=[-95, -85, -90]):
             self.assertRaises(ValueError)
 
 
 if __name__ == '__main__':
     unittest.main()
-
